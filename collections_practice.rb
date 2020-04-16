056c3a07ccbe667cf4238bb85cdf462eb3ea191d
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-16 15:00:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,17 +11,13 @@ def sort_array_char_count(arr)
   arr.sort { |a,b| a.length <=> b.length} 
 end
 
-def swap_elements(array, index, new_index)
-#  arr[1], arr[2] = arr[2], arr[1]
-#arr.swap_elements_from_to(arr,2,1)
 
-arr.swap!(2,1)
-  arr
-end
 
 def swap_elements(arr, index, new_index)
-  arr[index], arr[new_index] = arr[new_index], arr[index]
+ # arr[1], arr[2] = arr[2], arr[1]
+  arr.swap!(2,1)
    arr
+   
 end
 
 def reverse_array(arr)
